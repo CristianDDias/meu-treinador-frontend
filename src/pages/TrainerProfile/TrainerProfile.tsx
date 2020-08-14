b6e681942562796d875c0456a3fdfbb5699c1f0b
Cristian Dias <cristian.dias@sap.com>
2020-08-13 21:51:24 -0300

    Specialities test

--- src/pages/TrainerProfile/TrainerProfile.tsx
+++ src/pages/TrainerProfile/TrainerProfile.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { makeStyles, Theme } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
 import Box from "@material-ui/core/Box";
+import Chip from "@material-ui/core/Chip";
 import IconButton from "@material-ui/core/IconButton";
 import Rating from "@material-ui/lab/Rating";
 import Typography from "@material-ui/core/Typography";
@@ -54,6 +55,16 @@ const useStyles = makeStyles((theme: Theme) => ({
   contactButton: {
     padding: theme.spacing(1),
     margin: theme.spacing(0, 1),
+  },
+  specialities: {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(0.5),
+      color: theme.palette.primary.main,
+      background: `${theme.palette.primary.main}26`,
+    },
   },
 }));
 
@@ -136,8 +147,83 @@ export const TrainerProfile = () => {
 
       <Box className={classes.card} display="flex" flexDirection="column">
         <Typography className={classes.title}>Especialidades</Typography>
+
+        {/* 
+
+        yellow
+          background-color: rgb(255, 244, 229);
+          color: #ff9800;
+
+        red
+          background-color: #fdecea;
+          color: #f44336;
+
+        blue
+          background-color: rgb(232, 244, 253);
+          color: #2196f3;
+
+        green
+          background-color: rgb(237, 247, 237);
+          color: #4caf50;
+
+        */}
+
+        <div className={classes.specialities}>
+          <Chip label="Body Building" />
+          <Chip label="Body Composition" />
+          <Chip label="Body Sculpting" />
+          <Chip label="Body Transformation" />
+          <Chip label="Boxing" />
+          <Chip label="Calisthenics" />
+          <Chip label="Core Strength" />
+          <Chip label="Cross Training" />
+          <Chip label="Cycling" />
+          <Chip label="Functional Training" />
+          <Chip label="Gymnastics" />
+          <Chip label="HIIT" />
+          <Chip label="Metafit" />
+          <Chip label="Muscular Hypertrophy" />
+          <Chip label="Olympic Lifting" />
+          <Chip label="Performance" />
+          <Chip label="Pilates" />
+          <Chip label="Pre-Post Natal" />
+          <Chip label="Rehabilitation" />
+          <Chip label="Running" />
+          <Chip label="Seniors Specific" />
+          <Chip label="Sports Specific" />
+          <Chip label="Strength and Conditioning" />
+          <Chip label="Weight Loss" />
+          <Chip label="Wellness" />
+          <Chip label="Yoga" />
+        </div>
+      </Box>
+
+      <Box className={classes.card} display="flex" flexDirection="column">
+        <Typography className={classes.title}>Locais de treinamento</Typography>
         <Typography variant="body2" align="justify">
-          {trainer.description}
+          MAP
+        </Typography>
+      </Box>
+
+      <Box className={classes.card} display="flex" flexDirection="column">
+        <Typography className={classes.title}>
+          Horários de atendimento
+        </Typography>
+        <Typography variant="body2" align="justify">
+          <div>Segunda - 06:00 - 08:00 | 11:00 - 15:00 | 19:00 - 22:00</div>
+          <div>Terça - 08:00 - 22:00</div>
+          <div>Quarta - 06:00 - 21:00</div>
+          <div>Quinta - 06:00 - 08:00 | 11:00 - 15:00</div>
+          <div>Sexta - 07:00 - 21:00</div>
+          <div>Sábado - Informe-se sobre a disponibilidade</div>
+          <div>Domingo - Informe-se sobre a disponibilidade</div>
+        </Typography>
+      </Box>
+
+      <Box className={classes.card} display="flex" flexDirection="column">
+        <Typography className={classes.title}>Avaliações</Typography>
+        <Typography variant="body2" align="justify">
+          Tudo certo!
         </Typography>
       </Box>
     </div>
