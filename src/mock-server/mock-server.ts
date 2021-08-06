1383027f3a17905911483f44a345b58f4aba4048
Cristian Dias <c-2d@hotmail.com>
2021-08-06 18:32:39 -0300

    Fix

--- src/mock-server/mock-server.ts
+++ src/mock-server/mock-server.ts
@@ -3,7 +3,7 @@ import { setupWorker, rest } from 'msw';
 import { db } from './mock-database';
 
 export const mockServer = setupWorker(
-  rest.get('/api/v1/trainers', (req, res, ctx) => {
+  rest.get('*/api/v1/trainers', (req, res, ctx) => {
     const name = req.url.searchParams.get('name')?.toLowerCase();
     const trainers = db.trainers.filter((trainer) =>
       name ? trainer.name.toLowerCase().includes(name) : true
@@ -11,22 +11,22 @@ export const mockServer = setupWorker(
     return res(ctx.json(trainers), ctx.delay());
   }),
 
-  rest.get('/api/v1/trainers/:trainerId', (req, res, ctx) => {
+  rest.get('*/api/v1/trainers/:trainerId', (req, res, ctx) => {
     const trainer = db.trainers.find(({ id }) => id === req.params.trainerId);
     return res(ctx.status(trainer ? 200 : 404), ctx.json(trainer), ctx.delay());
   }),
 
-  rest.get('/api/v1/trainers/:trainerId/reviews', (req, res, ctx) => {
+  rest.get('*/api/v1/trainers/:trainerId/reviews', (req, res, ctx) => {
     const reviews = db.trainersReviews[req.params.trainerId] ?? [];
     return res(ctx.json(reviews), ctx.delay());
   }),
 
-  rest.get('/api/v1/users/:userId/favorite-trainers', (_req, res, ctx) => {
+  rest.get('*/api/v1/users/:userId/favorite-trainers', (_req, res, ctx) => {
     const trainers = db.favoriteTrainers.map((id) => db.trainers.find((trainer) => trainer.id === id));
     return res(ctx.json(trainers), ctx.delay());
   }),
 
-  rest.put('/api/v1/users/:userId/favorite-trainers', (req, res, ctx) => {
+  rest.put('*/api/v1/users/:userId/favorite-trainers', (req, res, ctx) => {
     const { isFavorite, trainerId } = req.body as any;
     if (isFavorite) {
       db.favoriteTrainers = db.favoriteTrainers.concat(trainerId);
